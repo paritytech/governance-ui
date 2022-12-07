50fc8b49d7b8e8f40ffd3cd3fd0ce429b8060561
hamidra <hamid.alipour@gmail.com>
2022-12-07 09:55:15 -0800

    make connect Modal stateful

--- src/components/connect.tsx
+++ src/components/connect.tsx
@@ -1,20 +1,10 @@
 import { BaseWallet } from '@polkadot-onboard/core';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAccount } from '../contexts/Account';
 import type { SigningAccount } from '../contexts/Account';
 import { useWallets } from '../contexts/Wallets';
 import { Button, Modal } from './common';
-const WalletsList = () => {
-  const { wallets, walletState, setWalletState } = useWallets();
-  const connectHandler = async (wallet: BaseWallet) => {
-    if (walletState[wallet?.metadata.title] == 'connected') {
-      await wallet.disconnect();
-      setWalletState(wallet?.metadata.title, 'disconnected');
-    } else {
-      await wallet.connect();
-      setWalletState(wallet?.metadata.title, 'connected');
-    }
-  };
+const WalletsList = ({ wallets, walletState, walletConnectHandler }) => {
   return (
     <div>
       {wallets?.map((wallet) => (
@@ -26,7 +16,7 @@ const WalletsList = () => {
             justifyContent: 'space-between',
           }}
         >
-          <div onClick={() => connectHandler(wallet)}>
+          <div onClick={() => walletConnectHandler(wallet)}>
             {`${wallet?.metadata?.title}`}
           </div>
           <div>
@@ -41,16 +31,10 @@ const WalletsList = () => {
     </div>
   );
 };
-const AccountList = () => {
-  const { connectedAccount, setConnectedAccount, walletsAccounts } =
-    useAccount();
-  const connectHandler = async (signingAccount: SigningAccount) => {
-    setConnectedAccount(signingAccount);
-  };
-  console.log(walletsAccounts);
+const AccountList = ({ accounts, connectedAccount, accountConnectHandler }) => {
   return (
     <div>
-      {Object.values(walletsAccounts).map((signingAccount) => (
+      {Object.values(accounts).map((signingAccount) => (
         // ToDo: add an account connect modal
         <div
           style={{
@@ -59,7 +43,7 @@ const AccountList = () => {
             justifyContent: 'space-between',
           }}
         >
-          <div onClick={() => connectHandler(signingAccount)}>
+          <div onClick={() => accountConnectHandler(signingAccount)}>
             {`${signingAccount.account.address}`}
           </div>
           <div>
@@ -75,22 +59,71 @@ const AccountList = () => {
   );
 };
 
+type ConnectViews = 'wallets' | 'accounts';
+
 const ConnectModal = () => {
   const [visible, setVisible] = useState(false);
-  const { walletsAccounts } = useAccount();
-  console.log(walletsAccounts);
-  const closeHandler = () => setVisible(false);
+  const [currentView, setCurrentView] = useState<ConnectViews>();
+  const { wallets, walletState, setWalletState } = useWallets();
+  const { connectedAccount, setConnectedAccount, walletsAccounts } =
+    useAccount();
 
+  const initialView: ConnectViews =
+    walletsAccounts && Object.keys(walletsAccounts).length > 0
+      ? 'accounts'
+      : 'wallets';
+  const toggleView = () => {
+    console.log(currentView);
+    setCurrentView((oldView) =>
+      oldView === 'wallets' ? 'accounts' : 'wallets'
+    );
+  };
+  const walletConnectHandler = async (wallet: BaseWallet) => {
+    if (walletState[wallet?.metadata.title] == 'connected') {
+      await wallet.disconnect();
+      setWalletState(wallet?.metadata.title, 'disconnected');
+    } else {
+      await wallet.connect();
+      setWalletState(wallet?.metadata.title, 'connected');
+    }
+  };
+  const accountConnectHandler = async (signingAccount: SigningAccount) => {
+    setConnectedAccount(signingAccount);
+    closeModal();
+  };
+  const closeModal = () => {
+    setVisible(false);
+  };
+  const openModal = () => {
+    setCurrentView(initialView);
+    setVisible(true);
+  };
+  useEffect(() => {
+    console.log('initiate');
+    setCurrentView(initialView);
+  }, [initialView]);
   return (
     <>
-      <Button color="secondary" onPress={() => setVisible(true)}>
+      <Button color="secondary" onPress={() => openModal()}>
         Connect
       </Button>
-      <Modal visible={visible} onClose={closeHandler}>
-        {walletsAccounts && Object.keys(walletsAccounts).length > 0 ? (
-          <AccountList />
-        ) : (
-          <WalletsList />
+      <Modal visible={visible} onClose={() => closeModal()}>
+        <div onClick={() => toggleView()}>{`${
+          currentView === 'accounts' ? 'wallets' : 'accounts'
+        }`}</div>
+        {currentView === 'accounts' && (
+          <AccountList
+            accounts={walletsAccounts}
+            connectedAccount={connectedAccount}
+            accountConnectHandler={accountConnectHandler}
+          />
+        )}
+        {currentView === 'wallets' && (
+          <WalletsList
+            wallets={wallets}
+            walletState={walletState}
+            walletConnectHandler={walletConnectHandler}
+          />
         )}
       </Modal>
     </>
