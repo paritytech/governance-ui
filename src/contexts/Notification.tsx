ecfc3fe82b224d808824197aafbd0aa782b8cb58
hamidra <hamid.alipour@gmail.com>
2022-12-26 20:48:30 -0800

    add comments

--- src/contexts/Notification.tsx
+++ src/contexts/Notification.tsx
@@ -18,10 +18,15 @@ const notificationContext = createContext({} as INotificationContext);
 export const useNotifications = () => useContext(notificationContext);
 
 const NotificationProvider = ({ children }: { children: React.ReactNode }) => {
+  // notification queue
   const [notifications, setNotifications] = useState<Array<INotification>>([]);
+
+  // adds a notification to the queue
   const notify = (notification: INotification) => {
     setNotifications((notifications) => [...notifications, notification]);
   };
+
+  // removes the next notification from the queue.
   const markAsRead = () => {
     console.log('read');
     setNotifications((notifications) => [...notifications.slice(1)]);
