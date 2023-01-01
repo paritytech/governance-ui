0c00b25fc1487274ef1f1fc8ad06be347c41c393
Julien Eluard <julien.eluard@gmail.com>
2023-01-01 15:22:29 +0100

    Cleanup

--- src/components/common/loading.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Loading as NextUILoading } from '@nextui-org/react';
-
-function Loading(): JSX.Element {
-  return <NextUILoading />;
-}
-
-export default Loading;
--- src/components/common/spacer.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Spacer as NextUISpacer } from '@nextui-org/react';
-
-function Spacer({ x = 1, y = 1 }: { x?: number; y?: number }): JSX.Element {
-  return <NextUISpacer x={x} y={y} />;
-}
-
-export default Spacer;
