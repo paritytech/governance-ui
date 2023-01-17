fa74a7bbbc550e3978d9d79c21332da20740922e
Julien Eluard <julien.eluard@gmail.com>
2023-01-17 20:27:00 +0100

    Cleanup

--- src/components/Referenda.tsx
+++ src/components/Referenda.tsx
@@ -5,7 +5,7 @@ import { useDrag } from '@use-gesture/react';
 import { createStandardAccountVote } from '../chain/conviction-voting';
 import { Card, Loading, Text } from '../ui/nextui';
 import { AccountVote, ReferendumOngoing, Track } from '../types';
-import { Network } from '../utils/polkadot-api';
+import { Network } from '../network';
 import { fetchReferenda, Post } from '../utils/polkassembly';
 import * as styles from './Referenda.module.css';
 
--- src/utils/polkadot-api.ts
+++ src/utils/polkadot-api.ts
@@ -1,41 +1,12 @@
-import { ApiPromise, WsProvider } from '@polkadot/api';
+import { ApiPromise } from '@polkadot/api';
+import { WsReconnectProvider } from './ws-reconnect-provider';
 
-// https://www.npmjs.com/package/@substrate/connect
-// https://github.com/polkadot-js/api/tree/master/packages/rpc-provider
-
-export enum Network {
-  Kusama = 'Kusama',
-  Polkadot = 'Polkadot',
-}
-
-function capitalizeFirstLetter(s: string): string {
-  return s.charAt(0).toUpperCase() + s.slice(1);
-}
-
-export namespace Network {
-  export function parse(network: string | null): Network {
-    return network
-      ? (capitalizeFirstLetter(network) as Network)
-      : (process.env.NETWORK &&
-          (capitalizeFirstLetter(process.env.NETWORK) as Network)) ||
-          Network.Polkadot;
-  }
-}
-
-export function networkFor(api: ApiPromise): Network {
-  return api.runtimeChain.toHuman() as Network;
-}
-
-export function endpointFor(network: Network): string {
-  switch (network) {
-    case Network.Kusama:
-      return 'wss://kusama-rpc.polkadot.io';
-    case Network.Polkadot:
-      return 'wss://rpc.polkadot.io';
-  }
-}
-
-export function newApi(endpoint: string): Promise<ApiPromise> {
-  const wsProvider = new WsProvider(endpoint);
-  return ApiPromise.create({ provider: wsProvider });
+export function newApi(endpoints: string[]): Promise<ApiPromise> {
+  const provider = new WsReconnectProvider(endpoints);
+  // TODO handle some failure/reconnection?
+  return ApiPromise.create({
+    provider,
+    noInitWarn: true,
+    throwOnConnect: false,
+  });
 }
