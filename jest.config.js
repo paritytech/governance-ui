9cfdae1548ef0b055d7045329dc9ca1faaae5251
Julien Eluard <julien.eluard@gmail.com>
2023-01-19 15:54:24 +0100

    Introduced jest testing

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+const config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/src/**/**.spec.ts'],
+};
+export default config;
---
+++ src/utils/set.spec.ts
@@ -0,0 +1,14 @@
+import { expect, test } from '@jest/globals';
+import { areEquals } from './set';
+
+test('empty sets are equals', () => {
+  const s1 = new Set([]);
+  const s2 = new Set([]);
+  expect(areEquals(s1, s2)).toBeTruthy();
+});
+
+test('sets are equals', () => {
+  const s1 = new Set([1, 2, 3]);
+  const s2 = new Set([3, 2, 1]);
+  expect(areEquals(s1, s2)).toBeTruthy();
+});
