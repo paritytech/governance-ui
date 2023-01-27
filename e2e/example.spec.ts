7b58ecb9ce2ff1b7f5f23457758f6af1c31d4fe1
hamidra <hamid.alipour@gmail.com>
2023-01-26 19:34:23 -0800

    add background deployments to gh action

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,18 +1,16 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+const endpoint = "http://127.0.0.1:1234/?rpc=ws://127.0.0.1:9984";
+test("has title", async ({ page }) => {
+  await page.goto(endpoint);
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Swipealot/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+/*test("get started link", async ({ page }) => {
+  await page.goto(endpoint);
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
-});
+  await page.getByRole("link", { name: "Get started" }).click();
+});*/
