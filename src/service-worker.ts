4cfdcdd165a756b677686b6daa4a712f3aa6c463
Julien Eluard <julien.eluard@gmail.com>
2023-04-11 10:23:10 +0200

    Simpler service worker

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,11 +1,8 @@
 import { manifest, version } from '@parcel/service-worker';
 
-const ASSETS_CACHE = `assets-version-${version}`;
-const ALL_CACHES = [ASSETS_CACHE];
-
 async function install() {
   // Cache all assets
-  const cache = await caches.open(ASSETS_CACHE);
+  const cache = await caches.open(version);
   const keys = await cache.keys();
   // Clear existing cached elements
   await Promise.all(keys.map((key) => cache.delete(key)));
@@ -14,33 +11,23 @@ async function install() {
 
 // First entry point, only called once per service worker version
 // See https://web.dev/service-worker-lifecycle/
-addEventListener('install', (e) => {
-  // Use latest version right away, even if some clients are using an older version
-  self.skipWaiting();
-
-  e.waitUntil(install());
-});
+addEventListener('install', (e) => e.waitUntil(install()));
 
 async function activate() {
-  // Delete older caches
   const keys = await caches.keys();
   await Promise.all(
-    keys.map((key) => !ALL_CACHES.includes(key) && caches.delete(key))
+    keys.map(key => key !== version && caches.delete(key))
   );
 }
 
-self.addEventListener('activate', (e: ExtendableEvent) => {
-  e.waitUntil(activate());
-  // Tell the active service worker to take control of the page immediately.
-  self.clients.claim();
-});
+addEventListener('activate', e => e.waitUntil(activate()));
 
 self.addEventListener('fetch', (event: FetchEvent) => {
   const url = new URL(event.request.url);
   if (url.origin == location.origin && manifest.includes(url.pathname)) {
     // Only consider cached assets
     event.respondWith(
-      caches.open(ASSETS_CACHE).then((cache) => {
+      caches.open(version).then((cache) => {
         return cache.match(event.request).then(async (response) => {
           if (response) {
             return response;
