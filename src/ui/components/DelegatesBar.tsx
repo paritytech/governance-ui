c74f90b5ebc13071d650f76e5ea85ab6105aeba4
Julien Eluard <julien.eluard@gmail.com>
2023-04-15 16:29:49 +0200

    Improve delegates list (fixes #398)

--- src/ui/components/DelegatesBar.tsx
+++ src/ui/components/DelegatesBar.tsx
@@ -3,10 +3,7 @@ import { Delegate, State } from '../../lifecycle/types.js';
 import { DelegateCard } from './delegation/DelegateCard.js';
 
 function filterVisibleDelegates(delegates: Delegate[]): Delegate[] {
-  const shuffledDelegates = new Array(...delegates).sort(
-    () => 0.5 - Math.random()
-  );
-  return shuffledDelegates.slice(0, 5);
+  return new Array(...delegates).sort(() => 0.5 - Math.random());
 }
 export function DelegatesBar({
   state,
@@ -22,9 +19,7 @@ export function DelegatesBar({
   return (
     <section className="flex w-full flex-col items-center justify-center gap-6 bg-gray-200 py-8 md:gap-12">
       <div className={`flex flex-col justify-center gap-0`}>
-        <span className="px-3 font-unbounded text-h4">
-          Choose a Worthy Delegate
-        </span>
+        <span className="px-3 font-unbounded text-h4">Volunteer Delegates</span>
       </div>
       <div className="flex max-w-full snap-x scroll-pl-6 gap-7 overflow-x-auto px-3 pb-1 lg:px-6	">
         {visibleDelegates.map((delegate, idx) => (
