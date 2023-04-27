24592214b4df4212969f6c5529975c0b7418a498
Julien <jeluard@users.noreply.github.com>
2023-04-27 06:49:46 -0700

    Moved to hash based location (#431)

--- src/ui/layouts/Delegation/DelegationLayout.tsx
+++ src/ui/layouts/Delegation/DelegationLayout.tsx
@@ -1,5 +1,6 @@
 import { useMemo } from 'react';
-import { Switch, Route, Redirect } from 'wouter';
+import { Switch, Route, Router, Redirect } from 'wouter';
+import { useLocationProperty, navigate } from 'wouter/use-location';
 import Footer from '../../components/Footer.js';
 import { Header, NotificationBox } from '../../components/index.js';
 import { DelegationPanel } from './DelegationPanel.js';
@@ -10,6 +11,19 @@ import {
   useAppLifeCycle,
   extractDelegatedTracks,
 } from '../../../lifecycle/index.js';
+
+function hashLocation() {
+  return window.location.hash.replace(/^#/, '') || '/';
+}
+
+function hashNavigate(path: string) {
+  navigate(`#${path}`);
+}
+
+function useHashLocation(): [string, (path: string) => void] {
+  const location = useLocationProperty(hashLocation);
+  return [location, hashNavigate];
+}
 
 export function DelegationLayout() {
   const { sectionRefs } = useDelegation();
@@ -26,20 +40,22 @@ export function DelegationLayout() {
         className="flex w-full flex-auto flex-col items-center justify-center gap-8 pb-48 pt-14 md:pt-20 lg:gap-16"
         ref={sectionRefs.get('top')}
       >
-        <Switch>
-          <Route path="/:address">
-            {({ address }) =>
-              address && isValidAddress(address) ? (
-                <SelectedDelegatePanel selectedDelegate={address} />
-              ) : (
-                <Redirect to={'/'} />
-              )
-            }
-          </Route>
-          <Route>
-            <DelegationPanel />
-          </Route>
-        </Switch>
+        <Router hook={useHashLocation}>
+          <Switch>
+            <Route path="/:address">
+              {({ address }: { address: string }) =>
+                address && isValidAddress(address) ? (
+                  <SelectedDelegatePanel selectedDelegate={address} />
+                ) : (
+                  <Redirect to={'/'} />
+                )
+              }
+            </Route>
+            <Route>
+              <DelegationPanel />
+            </Route>
+          </Switch>
+        </Router>
       </main>
       <Footer />
     </>
