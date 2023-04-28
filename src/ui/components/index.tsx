a9f9cd84293909c2a05aaf57523f61f96692a435
Julien Eluard <julien.eluard@gmail.com>
2023-04-28 10:12:35 +0200

    Cleanup

--- src/ui/components/index.tsx
+++ src/ui/components/index.tsx
@@ -3,5 +3,3 @@ export { ErrorBoundary } from './ErrorBoundary.js';
 export { Header } from './Header.js';
 export { LoadingPanel } from './Loading.js';
 export { NotificationBox } from './NotificationBox.js';
-export { ReferendaDeck } from './Referenda.js';
-export { VoteActionBar, VotesSummaryTable, VotingPanel } from './Voting.js';
