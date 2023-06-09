255fe849c8036af21cc716aeb2dc2bdbee7bebf0
Julien Eluard <julien.eluard@gmail.com>
2023-06-09 16:32:07 +0200

    Allow to copy address

--- src/ui/components/accounts/Accounticon.tsx
+++ src/ui/components/accounts/Accounticon.tsx
@@ -1,5 +1,6 @@
 import Identicon from '@polkadot/react-identicon';
 import { stringShorten } from '@polkadot/util';
+import { useAppLifeCycle } from 'src/lifecycle';
 
 export function Accounticon({
   address,
@@ -12,14 +13,20 @@ export function Accounticon({
   size?: number;
   textClassName?: string;
 }) {
+  const { updater } = useAppLifeCycle();
   const btnTitle = name || address;
   return (
     <div className="flex flex-nowrap items-center gap-2">
       <Identicon
-        style={{ cursor: 'unset' }}
         value={address}
         theme="polkadot"
         size={size || 18}
+        onCopy={() =>
+          updater.addReport({
+            type: 'Info',
+            message: 'Address copied to clipboard!',
+          })
+        }
       />
       <div className={`${textClassName || ''}`}>
         {stringShorten(btnTitle, 5)}
