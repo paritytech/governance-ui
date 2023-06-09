f0b69d24ded276e39835f1a5509d30b589015413
dnlklmn <pelicansinberlin@gmail.com>
2023-06-09 12:13:39 +0200

    Update Accounticon.tsx

--- src/ui/components/accounts/Accounticon.tsx
+++ src/ui/components/accounts/Accounticon.tsx
@@ -1,5 +1,6 @@
 import Identicon from '@polkadot/react-identicon';
 import { stringShorten } from '@polkadot/util';
+import { useState } from 'react';
 
 export function Accounticon({
   address,
@@ -13,17 +14,29 @@ export function Accounticon({
   textClassName?: string;
 }) {
   const btnTitle = name || address;
+  const [copied, setCopied] = useState(false);
   return (
-    <div className="flex flex-nowrap items-center gap-2">
+    <div
+      className="flex cursor-pointer flex-nowrap items-center gap-2"
+      onClick={() => {
+        navigator.clipboard.writeText(address);
+        setCopied(true);
+        setTimeout(() => setCopied(false), 2000);
+      }}
+    >
       <Identicon
         style={{ cursor: 'unset' }}
         value={address}
         theme="polkadot"
         size={size || 18}
       />
-      <div className={`${textClassName || ''}`}>
-        {stringShorten(btnTitle, 5)}
-      </div>
+      {copied ? (
+        <div className={`${textClassName || ''}`}>Copied to clipboard!</div>
+      ) : (
+        <div className={`${textClassName || ''}`}>
+          {stringShorten(btnTitle, 5)}
+        </div>
+      )}
     </div>
   );
 }
